6e3fcaafcc6a4365af4b194805bb051cc62e065b
touch <tttouch1122@gmail.com>
2022-12-23 15:42:43 +0700

    l2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
-double findGrade(double x){
+char findGrade(double x){
     char y;
     
 	if(x>90)
@@ -34,8 +34,10 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
+		getline(cin,name[i]);
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
 		cout << "Score of student " << i+1 << ": ";
+		cin>>score[i];
 		//[Missing Code 3] Get score of the i-th students.
 		i++;
 	}
